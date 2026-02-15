9d19a3e7a3ec6e3da87aa236d19ca53e9d50ca34
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2026-02-15 18:01:16 +0800

    feat: Remove service worker registration from entry client

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,13 +1,4 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
 
-// Register service worker and PWA
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(() => {
-      // SW registration failed, but continue anyway
-    });
-  });
-}
-
 mount(() => <StartClient />, document.getElementById("app")!);
